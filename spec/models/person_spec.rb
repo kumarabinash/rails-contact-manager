ae43dba0e43b72ff34349afdce3ed12155935597
Kumar Abinash <abinash.b2@gmail.com>
2015-06-24 13:01:02 +0530

    Confirmed person is valid in person spec

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -4,6 +4,11 @@ RSpec.describe Person, type: :model do
 	let(:person) do
 		Person.new(first_name: "Ramesh", last_name: "Narayan")
 	end
+
+	it 'is valid' do
+		expect(person).to be_valid
+	end
+
   it 'is invalid without a first_name' do
   	# person = Person.new(first_name: nil)
   	person.first_name = nil
